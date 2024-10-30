5165d04312121db00d444dee16379a4a78b337f8
Hello world <somnii006@gmail.com>
2024-10-30 12:33:26 -0300

    anda

---
+++ src/main/java/seahub/trabajofinaltorneo/logica/Grupo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package seahub.trabajofinaltorneo.logica;
+
+/**
+ *
+ * @author tinov
+ */
+public class Grupo {
+    
+}

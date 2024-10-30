186ae7a528856df42569d6f84e2770abf2029df5
Hello world <somnii006@gmail.com>
2024-10-29 23:12:38 -0300

    subiendo

--- target/generated-sources/annotations/seahub/trabajofinaltorneo/logica/Etapa_.java
+++ target/generated-sources/annotations/seahub/trabajofinaltorneo/logica/Etapa_.java
@@ -10,7 +10,7 @@ import seahub.trabajofinaltorneo.logica.Participante;
 import seahub.trabajofinaltorneo.logica.ParticipanteEtapa;
 import seahub.trabajofinaltorneo.logica.Torneo;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-29T23:00:04", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-29T23:08:50", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Etapa.class)
 public class Etapa_ { 
 
